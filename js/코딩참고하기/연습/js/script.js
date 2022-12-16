4ee6b3c3c88c3fb986f2203f11f07a8488443eb3
hazeroyoon <114642651+hazeroyoon@users.noreply.github.com>
2022-12-16 10:08:12 +0900

    1.1

--- js/코딩참고하기/연습/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('.your-class').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    prevArrow:"<i class='prev-arrow fa-solid fa-circle-chevron-left'></i>",  //이전 화살표 모양 설정
-    nextArrow:"<i class='next-arrow fa-solid fa-circle-chevron-right'></i>", //다음 화살표 모양 설정 
-  });
-});
